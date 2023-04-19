5be721dcd64b3a01050eac43741469a3551780e2
Brian Connolly <brian.connolly71@gmail.com>
2023-04-19 21:26:33 +0100

    try and fail with eureka

--- src/main/java/com/msa/assignment/productservice/ProductServiceSpringApp.java
+++ src/main/java/com/msa/assignment/productservice/ProductServiceSpringApp.java
@@ -2,8 +2,10 @@ package com.msa.assignment.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductServiceSpringApp {
     public static void main(String[] args){
         SpringApplication.run(ProductServiceSpringApp.class, args);
